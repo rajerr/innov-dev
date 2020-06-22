74646c611cd2e75934d4b3063593d52711e222e0
rajerr <rajerr2013@gmail.com>
2020-06-22 14:15:08 +0000

    L'architecture du projet

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Accueil</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="public/style/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row bg-info">
+        <h2 class="col-12 text-light text-center mt-5">ce Site est en phase de développement</h2>
+            <marquee class="col-12 text-light" behavior="scroll" direction="left" scrollamount="10">Team Galsen</marquee>
+            <marquee class="col-12 text-light" behavior="scroll" direction="right" scrollamount="20"><img class="img-fluid. max-width: 100% height: auto" src="public\img\galsen.jpg" alt="" srcset=""></marquee>
+            <marquee class="col-12 text-light" behavior="scroll" direction="left" scrollamount="20"><img class="img-fluid. max-width: 100% height: auto" src="public\img\rca.jpg" alt="" srcset=""></marquee>
+            <marquee class="col-12 text-light" behavior="scroll" direction="right" scrollamount="10">Team Central African Republic</marquee>
+        </div> 
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
